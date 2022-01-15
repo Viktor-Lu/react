95face07001ed79d8694997034650cb46012f0c7
Viktor-Lu <Eco-superstar@mail.ru>
2022-01-15 11:32:30 +0300

    Дробление Соц.сети на компоненты 12 урок

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Header =()=> {
+return(
+  <header className='header'>
+    <img src='https://say-hi.me/wp-content/uploads/2020/02/AI-logo-maker.jpg'/>
+  </header>
+
+)
+}
+
+export default Header;
