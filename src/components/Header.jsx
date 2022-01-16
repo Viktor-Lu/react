e4d6babbfcc4a8483f823ec2888a4693c9875b79
Viktor-Lu <Eco-superstar@mail.ru>
2022-01-16 20:00:53 +0300

    14 урок CSS-modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import classes from './Header.module.css';
+
 const Header =()=> {
 return(
-  <header className='header'>
+  <header className={classes.header}>
     <img src='https://say-hi.me/wp-content/uploads/2020/02/AI-logo-maker.jpg'/>
   </header>
 
