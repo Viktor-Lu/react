40207c79b506dbc84d93de564fb8a4866235a388
Viktor-Lu <Eco-superstar@mail.ru>
2022-01-18 21:31:54 +0300

    Добавление сердечка в Like.jsx

--- src/components/Profile/MyPosts/Post/like/like.jsx
+++ src/components/Profile/MyPosts/Post/like/like.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import classes from './like.css';
+import classes from './like.module.css';
 
 const Like = (props) => {
   return (
   <div className ={classes.like}>
+
+  <img src='https://cdn-icons-png.flaticon.com/512/2107/2107845.png'></img>
 
   {props.num}
 
