a8b5297aff4e250a82d71eb7f25ad9370ca7d101
Viktor-Lu <Eco-superstar@mail.ru>
2022-01-26 15:26:02 +0300

    21 урок.Вёрстка Dialogs[C

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
 
-const Dialogs =()=> {
-  return(
-    <div>
-    Dialogs
-    </div>
+const Dialogs = () => {
+    return (
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={`${classes.dialogsItems} ${classes.active}`}>
+                    Andrew
+                </div>
+                <div className={classes.dialog}>
+                    Dmitry
+                </div>
 
-  )
+                <div className={classes.dialog}>
+                    Sasha
+                </div>
+
+                <div className={classes.dialog}>
+                    Sveta
+                </div>
+
+                <div className={classes.dialog}>
+                    Valera
+                </div>
+
+                <div className={classes.dialog}>
+                    Viktor
+                </div>
+
+            </div>
+
+
+            <div className={classes.message}>
+                <div className={classes.message}>One</div>
+                <div className={classes.message}>Two</div>
+                <div className={classes.message}>Free</div>
+
+            </div>
+
+
+        </div>
+
+    )
 }
 
 export default Dialogs;
