83b0b5dbe644103598aefb71a5b5904a71c23053
Viktor-Lu <Eco-superstar@mail.ru>
2022-01-27 13:02:09 +0300

    22 Урок.Route exact

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <div className={`${classes.dialogsItems} ${classes.active}`}>
-                    Andrew
+                <div className={classes.dialogsItems}>
+                    <NavLink activeClassName={classes.activeLink}  to='/dialogs/1'>Andrew</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Dmitry
-                </div>
-
-                <div className={classes.dialog}>
-                    Sasha
+                    <NavLink activeClassName={classes.activeLink} to='/dialogs/2'>Dmitry</NavLink>
                 </div>
 
                 <div className={classes.dialog}>
-                    Sveta
+                    <NavLink activeClassName={classes.activeLink} to='/dialogs/3'>Sasha</NavLink>
                 </div>
 
                 <div className={classes.dialog}>
-                    Valera
+                    <NavLink activeClassName={classes.activeLink} to='/dialogs/4'>Sveta</NavLink>
                 </div>
 
                 <div className={classes.dialog}>
-                    Viktor
+                    <NavLink activeClassName={classes.activeLink} to='/dialogs/5'>Valera</NavLink>
+                </div>
+
+                <div className={classes.dialog}>
+                    <NavLink activeClassName={classes.activeLink} to='/dialogs/6'>Viktor</NavLink>
                 </div>
 
             </div>
